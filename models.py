07e5457bac808cbb90e06e35e7dca19cb9534c51
mcforema <mcforema@asu.edu>
2020-03-29 17:21:04 -0700

    Questions instead of stories

--- models.py
+++ models.py
@@ -1,27 +1,46 @@
 from dallinger.nodes import Source
-import random
+from dallinger.models import questions
 
 
-class WarOfTheGhostsSource(Source):
-    """A Source that reads in a random story from a file and transmits it."""
+class Questionaire(Source):
+    """A Source that reads in a question from a file and transmits it."""
 
-    __mapper_args__ = {"polymorphic_identity": "war_of_the_ghosts_source"}
+    __mapper_args__ = {"polymorphic_identity": "questionaire_source"}
 
     def _contents(self):
         """Define the contents of new Infos.
 
         transmit() -> _what() -> create_information() -> _contents().
         """
-        stories = [
-            "ghosts.md",
-            "cricket.md",
-            "moochi.md",
-            "outwit.md",
-            "raid.md",
-            "species.md",
-            "tennis.md",
-            "vagabond.md",
-        ]
-        story = random.choice(stories)
+           story = random.choice(stories)
         with open("static/stimuli/{}".format(story), "r") as f:
             return f.read()
+
+number_transmissions = len(self.infos())
+        import json
+        questions = [
+            json.dumps({
+                'question': 'What is the capital city of France?',
+                'number': 'practice 1',
+                'round': 0,
+                'topic': 'Practice',
+                'Wwer': 'Barcelona',
+                'Rwer': 'Paris',
+                'pic': False,
+                }),
+            json.dumps({
+                'question': 'How much does an average chimpanzee weigh?',
+                'number': 'practice 2',
+                'round': 0,
+                'topic': 'Practice',
+                'Wwer': '500kg',
+                'Rwer': '50kg',
+                'pic': False,
+            })
+        ]
+number_transmissions = len([i for i in self.infos(questions) if i.contents not in ["Bad Luck", "Good Luck"]])
+        if number_transmissions < len(questions):
+            question = questions[number_transmissions]
+        else:
+            question = questions[-1]
+        return question
