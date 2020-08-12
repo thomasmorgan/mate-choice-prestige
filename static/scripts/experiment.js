7d069739c96a1575432e43257d8cd914006c03c9
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-08-12 09:39:54 -0700

    Programatically create age options

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -223,25 +223,11 @@ function submit_final_response(response) {
   });
 }
 
-var age = document.getElementById("age");
-
-//Create array of options to be added
-var array = ["18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", 
-  "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", 
-  "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", 
-  "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", 
-  "96", "97", "98", "99", "100"];
-
-//Create and append select list
-var selectList = document.createElement("select");
-selectList.setAttribute("id", "mySelect");
-myDiv.appendChild(selectList);
-
-//Create and append the options
-for (var i = 0; i < array.length; i++) {
-    var option = document.createElement("option");
-    option.setAttribute("value", array[i]);
-    option.text = array[i];
-    selectList.appendChild(option);
-}
+window.onload = function() {
+  if ($('#age').length) {
+    for (i=18; i<101; i++) {
+      $("#age").append('<option value="' + i + '">' + i + '</option>');
+    }
+  }
+};
 
